58dbf6b1759b33e798c5f0af376e4db28be23566
Julie <115560412+sutabasutoo@users.noreply.github.com>
2024-12-11 18:05:34 +0100

    Add files via upload

---
+++ Assets/Scripts/GetMainLightDirection.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GetMainLightDirection : MonoBehaviour{
+    [SerializeField] private Material skyboxMaterial;
+
+    private void Update() {
+        skyboxMaterial.SetVector("_MainLightDirection", transform.forward);
+    }
+}
